2c01c7063bd95294ad1c5c1ec0f022cf2460df93
Wyatt Johnson <wyattjoh@gmail.com>
2014-11-11 16:03:30 -0700

    Fixed problems with setup script

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -5,6 +5,10 @@
     var ListingCtrl = function($http) {
         var vm = this;
 
+        // Set hostname
+        vm.host = window.location.host;
+
+        // Fetch the resources
         $http.get("/data/resources.json")
             .success(function(response) {
                 vm.resources = response.data;
@@ -16,5 +20,17 @@
     };
 
     app.controller("ListingCtrl", ListingCtrl);
+
+    window.selectText = function(elem) {
+        if (document.selection) {
+            var range = document.body.createTextRange();
+            range.moveToElementText(elem);
+            range.select();
+        } else if (window.getSelection) {
+            var range = document.createRange();
+            range.selectNode(elem);
+            window.getSelection().addRange(range);
+        }
+    };
 
 })();
