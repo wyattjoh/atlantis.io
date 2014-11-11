a79990f2c4884a0912f944226149fd933e5d9906
Wyatt Johnson <wyattjoh@gmail.com>
2014-11-11 11:39:40 -0700

    Made changes to requests

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -6,17 +6,10 @@
         var vm = this;
 
         $http.get("/data/resources.json")
-            .success(function(data) {
-                vm.resources = data;
-                vm.resources_string = _.pluck(data, 'url').join(" ");
-            })
-            .error(function(err) {
-                console.error(err);
-            });
-
-        $http.get("/data/config.json")
-            .success(function(data) {
-                vm.config = data;
+            .success(function(response) {
+                vm.resources = response.data;
+                vm.resources_string = _.pluck(response.data, 'url').join(" ");
+                vm.config = response;
             })
             .error(function(err) {
                 console.error(err);
